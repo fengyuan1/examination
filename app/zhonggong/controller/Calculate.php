12f2ef8957a409ad87ceb239ca2f4a7e9780f968
fengyuan@ifreegroup.com <939231156@qq.com>
2020-07-30 18:09:22 +0800

    ceshi

--- app/zhonggong/controller/Calculate.php
+++ app/zhonggong/controller/Calculate.php
@@ -17,7 +17,6 @@ class Calculate extends Controller
       $data=Db::name('institution')->order('data desc')->where('area_code','=',null)->select()->toArray();
 
       foreach ($data as $k=>$v ){
-
           $area=Db::name('wy_area')->select()->toArray();
            foreach ($area as $kk=>$vo){
                if(strpos($v['title'],$vo['name'])!==false){
@@ -28,10 +27,32 @@ class Calculate extends Controller
                    echo '匹配成功';
                }
            }
-
       }
 
+      $this->replenish();
     }
 
+    /**
+     * 补充
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function replenish(){
+        $data=Db::name('institution')->order('data desc')->where('area_code','=',null)->select()->toArray();
+
+        foreach ($data as $k=>$v ){
+            $area=Db::name('wy_area')->select()->toArray();
+            foreach ($area as $kk=>$vo){
+                if(strpos($v['title'],substr($vo['name'],0,strlen($vo['name'])-3)!==false)){
+                    Db::name('institution')->where('id',$v['id'])->update([
+                        'area_code'=>$vo['code'],
+                        'area_name'=>$vo['name'],
+                    ]);
+                    echo '匹配成功';
+                }
+            }
+        }
+    }
 
 }
